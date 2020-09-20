9c9199f88823a86a643e91b9bc7733c65435bd2f
Richard Brooks <richard.brooks@causeway.com>
2020-09-20 21:52:22 +0100

    Updated README.md Added server.port for AWS Corrected test class package

--- src/test/java/biggusrichus/locationapi/DemoApplicationTests.java
+++ src/test/java/biggusrichus/locationapi/DemoApplicationTests.java
@@ -1,9 +1,9 @@
-package com.example.demo;
+package biggusrichus.locationapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = biggusrichus.locationapi.DemoApplication.class)
 class DemoApplicationTests {
 
 	@Test
